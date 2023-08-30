78d98a6fc988eccb6f86e71477d61a7508f21261
kangsikWoo <wks09254930@gmail.com>
2023-08-30 15:12:29 +0900

    어드민 사용자 API 만듦

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FiShoppingBag } from 'react-icons/fi';
-import { BsFillPencilFill } from 'react-icons/bs';
-import { login, logout, onUserStateChange } from '../api/firebase';
-import User from './User';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+import { BsFillPencilFill } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../api/firebase";
+import User from "./User";
 
 export default function Navbar() {
   const [user, setUser] = useState();
 
   useEffect(() => {
-    onUserStateChange(setUser);
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    });
   }, []);
 
   return (
-    <header className='flex justify-between border-b border-gray-300 p-2'>
-      <Link to='/' className='flex items-center text-4xl text-brand'>
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
         <FiShoppingBag />
         <h1>Shoppy</h1>
       </Link>
-      <nav className='flex items-center gap-4 font-semibold'>
-        <Link to='/products'>Products</Link>
-        <Link to='/carts'>Carts</Link>
-        <Link to='/products/new' className='text-2xl'>
+      <nav className="flex items-center gap-4 font-semibold">
+        <Link to="/products">Products</Link>
+        <Link to="/carts">Carts</Link>
+        <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill />
         </Link>
         {user && <User user={user} />}
